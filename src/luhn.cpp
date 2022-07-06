c68b8698704d1145410233ba9d9038a48c8a1071
John Roughley <jroughley@softwareinstitute.com>
2022-07-06 15:17:12 +0100

    algorithm reverse iterates through the input string

--- src/luhn.cpp
+++ src/luhn.cpp
@@ -10,12 +10,13 @@ bool _Luhn::Validate(std::string&& input)
     (
         std::remove_if(input.begin(), input.end(), [](const char& c){ return !isdigit(c); }) , input.end()
     );
+    std::reverse(input.begin(), input.end());
 
     int sum{0}, index{0};
     for(char c : input)
     {
         int i{c - '0'}; // cast char digit to int
-        (index%2 == 0) // doubling check applied to alternating digits starting with first
+        (index%2 == 1) // doubling check applied to alternating digits, starting from rhs of original input
             ? (sum += (i*2 > 9) ? i*2 - 9 : i*2)
             : sum += i;
         ++index;
